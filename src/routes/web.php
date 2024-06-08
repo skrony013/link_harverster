cc544a40910f3650c1ae8a3031c619fc491c9930
MD RONY AHMED <mdronyahmed013@gmail.com>
2024-06-08 22:03:21 +0600

    routes update and UrlController created

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UrlController;
 
-Route::get('/', function () {
-    return view('frontend/index');
+Route::get('/clear', function() {
+    Artisan::call('cache:clear');
+    Artisan::call('config:clear');
+    Artisan::call('config:cache');
+    Artisan::call('view:clear');
+    return "Cleared!";
 });
+
+
+// Frontend Routes  Start Here
+
+Route::get('/', [UrlController::class, 'index'])->name('index');
